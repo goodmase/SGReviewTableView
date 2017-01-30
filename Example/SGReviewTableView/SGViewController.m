5a246ae4a6017ecc25bcb68af990458c7cd036f8
Stephen Goodman <stephen.goodman@gmail.com>
2017-01-30 15:10:41 -0600

    hide the status bar. Add nav title.

--- Example/SGReviewTableView/SGViewController.m
+++ Example/SGReviewTableView/SGViewController.m
@@ -16,12 +16,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.navigationItem.title = @"Reviews";
     // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(BOOL)prefersStatusBarHidden
+{
+    return YES;
 }
 
 /*
